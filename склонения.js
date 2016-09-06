fa402297b9f5ca41d4fdd54f72fa0c52260e4719
Vladimir Shenberg <shenberg.v@ya.ru>
2016-09-06 18:32:02 +0400

    Функция склонения. Входные данные число, массив. Например: func(128, ["программист","программиста","программистов"]); // 128 программистов

---
+++ склонения.js
@@ -0,0 +1,22 @@
+﻿function end(number, endingArr){
+    var i, 
+        ending, 
+        currentNum = number;
+    
+    number = number % 100;
+    
+    if (number >= 11 && number <= 19) {
+        ending = currentNum + " " + endingArr[2];
+    } else {
+        i = number % 10;
+        
+        switch (i) {
+            case (1): ending = currentNum + " " + endingArr[0]; break;
+            case (2):
+            case (3):
+            case (4): ending = currentNum + " " + endingArr[1]; break;
+            default: ending = currentNum + " " + endingArr[2];
+        }
+    }
+    return ending;
+}
